50d9665eb456669d500e5d2948aa39374e4158eb
alexjbustillos <alexjbustillos@gmail.com>
2020-11-12 15:01:33 -0500

    [export]modules

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { add, subtract } = require("./myModule");
+
 let name = "Alex Bustillos"
 console.log(name);
 
@@ -5,3 +7,6 @@ printName = (person) => {
     return `Hello, ${person}`;
 }
 console.log(printName(name));
+
+console.log(add(5, 50));
+console.log(subtract(10, 20));
---
+++ myModule.js
@@ -0,0 +1,15 @@
+beBasic = () => "That's so fetch!";
+
+add = (num1, num2) => {
+    return num1 + num2;
+};
+
+subtract = (num1, num2) => {
+    return num2 - num1;
+};
+
+module.exports = {
+    beBasic,
+    add,
+    subtract
+};
